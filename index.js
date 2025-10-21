1f9dfac2f19fe5fa612e1bf118206ae13a08eb8f
Aswinchandran-6649 <aswinchandran6649@gmail.com>
2025-10-21 12:33:42 +0530

    change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const jsonServer = require('json-server')
 
 //create server instance
-const GardenServer = jsonServer.create()
+const inventory = jsonServer.create()
 
 //tell json-server to use db.json as data source
 const router = jsonServer.router('db.json')
@@ -11,15 +11,15 @@ const router = jsonServer.router('db.json')
 const middleware = jsonServer.defaults()
 
 //tell the server to use router and middleware
-GardenServer.use(middleware)
-GardenServer.use(router)
+inventory.use(middleware)
+inventory.use(router)
 
 
 //set the port, if locally running use port 3000
 const PORT = process.env.PORT || 3000
 
 //define the port in which server has to run
-GardenServer.listen(PORT, ()=>{
+inventory.listen(PORT, ()=>{
     console.log(`rbServer running at port ${PORT} and waiting for client request...`);
 })
 //in package.json, in scripts change test to, "start": "node index.js"
